5c94b75be02d3acde731c621d5aba9cbef7da770
Narotam-Mishra <narotam21jan@gmail.com>
2025-01-11 23:57:34 +0530

    nextjs_portfolio_Header_design_p1_1.6

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
+        </div>
+    </div>
+  )
+}
+
+export default Header
