3cc159bc39a99091a01d1414bffeffa0d661a868
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-05 22:55:53 +0530

    nextjs_portfolio_added_Header_p2_1.7

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,11 +5,26 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <div>
-            <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
-        </div>
+      <div>
+        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi! I am William Mark{" "}
+        <Image src={assets.hand_icon} alt="" className="w-6" />{" "}
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+        full stack web developer based in India
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        I am a full stack developer from California, USA with 10 years of
+        experience in multiple companies like Tesla, Microsoft and Apple.
+      </p>
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href="#contact" className='px-10 py-3 border border-white rounded-full flex  bg-black text-white items-center gap-2'>contact me <Image src={assets.right_arrow_white} alt="" className="w-4" /></a>
+        <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>my resume <Image src={assets.download_icon} alt="" className="w-4" /></a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
